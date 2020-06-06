6a732007696f0a708284e3ab51fed54d855f71c4
Malongyang <1430634889@qq.com>
2020-06-06 20:18:45 +0800

    完成注册页

--- assets/js/login.js
+++ assets/js/login.js
@@ -22,12 +22,54 @@ $(function () {
             url: 'http://www.liulongbin.top:3007/api/reguser',
             data: data,
             success: function (back) {
-                alert(back.message);
+                //提示层
+                layer.msg(back.message);
+                // alert(back.message);
                 if (back.status === 0) {
                     $('#register').hide().prev().show();
                 }
             }
         })
-    })
+    });
 
+
+    // 注册登录页面
+    $('#login form').on('submit', function (e) {
+        // 阻止默认提交
+        e.preventDefault();
+        // 获取用户名和密码
+        var data = $(this).serialize();
+        // 发送ajax请求
+        $.ajax({
+            type: 'post',
+            url: 'http://www.liulongbin.top:3007/api/login',
+            data: data,
+            success: function (back) {
+                //提示层
+                layer.msg(back.message);
+                // alert(back.message);
+                if (back.status === 0) {
+                    // 保存令牌token
+                    localStorage.setItem('token', back.token);
+                    //调转到index.html
+                    location.href = '/index.html'
+                }
+
+            }
+        })
+    });
+
+    // 表单验证
+
+    // 加载form模块
+    var form = layui.form;
+    // 调用form.verify()方法
+    form.verify({
+        len: [/^[\S]{6,12}$/, '密码长度不对'],
+        // len: function (val) {
+        //     if (val.trim().length < 6 || val.trim().length > 12) {
+        //         return "密码不对"
+        //     }
+        // }
+    })
 })
